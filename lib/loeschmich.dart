5228edf2aa119bbf4fc4faa08a7c22b0a3563604
Pascal Cantaluppi <pascal@cantaluppi.ch>
2024-04-20 08:53:01 +0200

    Firebase example

---
+++ lib/loeschmich.dart
@@ -0,0 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textFieldController = TextEditingController();
+
+  void _addItem() {
+    if (_textFieldController.text.isNotEmpty) {
+      FirebaseFirestore.instance.collection('items').add({
+        'title': _textFieldController.text,
+      });
+      _textFieldController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Firestore Example'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('items').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Text('Something went wrong');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data['title']),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(
+                labelText: 'New Item',
+              ),
+              onSubmitted: (_) => _addItem(),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addItem,
+        tooltip: 'Add Item',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,81 @@
-// main.dart
-import 'dart:async';
+// // main.dart
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'components/splash.dart';
+// import 'components/login.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   try {
+//     await dotenv.load(fileName: ".env");
+//   } catch (e) {
+//     // ignore: avoid_print
+//     print('!!!! Failed to load env variables: $e');
+//   }
+
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // Initialize Supabase
+//   Future<void> initializeSupabase() async {
+//     final String supabaseUrl = dotenv.env['URL'] ?? '';
+//     final String supabaseAnonKey = dotenv.env['KEY'] ?? '';
+//     await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: FutureBuilder(
+//         // Initialize Supabase
+//         future: Future.wait([
+//           initializeSupabase(),
+//           // Timer for splash screen
+//           Future.delayed(const Duration(seconds: 4)),
+//         ]),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             return const LoginPage();
+//           } else {
+//             return const SplashScreen();
+//           }
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'components/splash.dart';
-import 'components/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:train_tracker/loeschmich.dart';
+import 'firebase_options.dart'; // Your Firebase configuration file
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await dotenv.load(fileName: ".env");
-  } catch (e) {
-    // ignore: avoid_print
-    print('!!!! Failed to load env variables: $e');
-  }
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // Initialize Supabase
-  Future<void> initializeSupabase() async {
-    final String supabaseUrl = dotenv.env['URL'] ?? '';
-    final String supabaseAnonKey = dotenv.env['KEY'] ?? '';
-    await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-        // Initialize Supabase
-        future: Future.wait([
-          initializeSupabase(),
-          // Timer for splash screen
-          Future.delayed(const Duration(seconds: 4)),
-        ]),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const LoginPage();
-          } else {
-            return const SplashScreen();
-          }
-        },
-      ),
+      title: 'Firebase Example',
+      home: MyHomePage(),
     );
   }
 }
