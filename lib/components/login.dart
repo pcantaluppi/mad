6341b6784f0901802da57b0b987d4f0645d23517
Pascal Cantaluppi <57313326+pcantaluppi@users.noreply.github.com>
2024-05-27 06:11:38 +0200

    Highest stop id

--- lib/components/login.dart
+++ lib/components/login.dart
@@ -108,7 +108,7 @@ class _LoginPageState extends State<LoginPage> {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => const PasswordReset(),
+                                  builder: (context) => PasswordReset(),
                                 ),
                               );
                             },
