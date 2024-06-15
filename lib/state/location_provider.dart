2c31d214c486ca85c235d0c8579795789dfa3916
Pascal Cantaluppi <57313326+pcantaluppi@users.noreply.github.com>
2024-06-15 18:24:13 +0200

    Update comments

--- lib/state/location_provider.dart
+++ lib/state/location_provider.dart
@@ -3,11 +3,17 @@
 import 'package:flutter/material.dart';
 import 'models/location_model.dart';
 
+/// A provider class for managing and notifying listeners about location data.
 class LocationProvider with ChangeNotifier {
+  /// A private list to store location data.
   List<LocationModel> _locations = [];
 
+  /// A getter to access the list of locations.
   List<LocationModel> get locations => _locations;
 
+  /// Sets the list of locations and notifies listeners about the change.
+  ///
+  /// [locations] is the new list of locations to be set.
   void setLocations(List<LocationModel> locations) {
     _locations = locations;
     notifyListeners();
