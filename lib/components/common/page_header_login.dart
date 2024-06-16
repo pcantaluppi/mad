a33c9ea5150393a6760a4b3f790df6610865c3c4
dany <danyyil.luntovsky@softrain.ch>
2024-06-16 14:47:42 +0200

    ipad header, dynamic map view

--- lib/components/common/page_header_login.dart
+++ lib/components/common/page_header_login.dart
@@ -1,14 +1,23 @@
-//page_header_login.dart
 import 'package:flutter/material.dart';
 
-/// A widget that represents the header of a login page.
 class PageHeaderLogin extends StatelessWidget {
   const PageHeaderLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Image.asset('assets/images/login-header.png'),
+    final screenWidth = MediaQuery.of(context).size.width;
+    const tabletBreakpoint = 600.0;
+
+    return Container(
+      color: Colors.white, // Set white background for the Container
+      child: SizedBox(
+        child: Image.asset(
+          screenWidth > tabletBreakpoint
+              ? 'assets/images/login-header-horizontal.png'
+              : 'assets/images/login-header.png',
+          fit: BoxFit.fill,
+        ),
+      ),
     );
   }
 }
